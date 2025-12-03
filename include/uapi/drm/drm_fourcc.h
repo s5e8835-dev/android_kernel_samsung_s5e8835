1a33a5020af5cc5ca57047232c9a703b01613836
ExtremeXT <extremextdev@gmail.com>
2025-12-03 13:59:31 +0200

    include: drm: Add missing DRM_FORMAT_P010_YVU definition
    Change-Id: I2b2435bdc3218ee12e69124b5149254db128fd6e
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- include/uapi/drm/drm_fourcc.h
+++ include/uapi/drm/drm_fourcc.h
@@ -351,6 +351,7 @@ extern "C" {
 #define DRM_FORMAT_YUV444	fourcc_code('Y', 'U', '2', '4') /* non-subsampled Cb (1) and Cr (2) planes */
 #define DRM_FORMAT_YVU444	fourcc_code('Y', 'V', '2', '4') /* non-subsampled Cr (1) and Cb (2) planes */
 
+#define DRM_FORMAT_P010_YVU fourcc_code('P', 'M', '2', '1')
 
 /*
  * Format Modifiers:
